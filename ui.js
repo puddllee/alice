fd5b26f317b7d49d606adbf8856083afcce4f049
Jake Runzer <jakerunzer@gmail.com>
2015-10-04 17:16:40 -0700

    add basic tabs

--- ui.js
+++ ui.js
@@ -1,3 +1,16 @@
 startUI = function () {
   console.log('starting ui');
+
+  makeCommentDiv();
+
+  chrome.extension.getURL('public/materialize.min.js');
+}
+
+var makeCommentDiv = function () {
+  $.get(chrome.extension.getURL('public/comment-box.html'), function (data) {
+    // $($.parseHTML(data)).appendTo('body');
+    var box = Boundary.createBox('comment-box').html(data);
+    Boundary.loadBoxCSS('#comment-box', chrome.extension.getURL('public/pure.min.css'));
+    Boundary.loadBoxCSS('#comment-box', chrome.extension.getURL('public/comment-box.css'));
+  });
 }
