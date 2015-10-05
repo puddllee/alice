3118a6ce075406f55f007d09fd5bdb4a161af19c
Jake Runzer <jakerunzer@gmail.com>
2015-10-04 18:27:08 -0700

    got data

--- ui.js
+++ ui.js
@@ -1,16 +1,53 @@
+var WRAPPER_BOX = 'wrapper-box';
+var LOADING_BOX = 'loading-box';
+var COMMENT_BOX = 'comment-box';
+
+// currently fetching places where this url has been posted to reddit
+// default to true because that is the inital state
+var loading = true;
+
 startUI = function () {
   console.log('starting ui');
 
-  makeCommentDiv();
-
-  chrome.extension.getURL('public/materialize.min.js');
+  makeLoadingBox();
 }
 
-var makeCommentDiv = function () {
+loadPlacesLinked = function (places) {
+  $('#' + LOADING_BOX).remove();
+  makeCommentDiv(places);
+}
+
+var makeLoadingBox = function () {
+  if (loading) {
+    $.get(chrome.extension.getURL('public/loading-box.html'), function (data) {
+      var lBox = Boundary.createBox(LOADING_BOX).html(data);
+      Boundary.loadBoxCSS('#' + LOADING_BOX, chrome.extension.getURL('public/pure.min.css'));
+      Boundary.loadBoxCSS('#' + LOADING_BOX, chrome.extension.getURL('public/loading.css'));
+    });
+  }
+}
+
+var toggleLoading = function () {
+  loading = !loading;
+  if (loading) {
+    attachLoadingBox();
+  } else {
+    $('#loading').remove();
+  }
+}
+
+var makeCommentDiv = function (places) {
   $.get(chrome.extension.getURL('public/comment-box.html'), function (data) {
-    // $($.parseHTML(data)).appendTo('body');
-    var box = Boundary.createBox('comment-box').html(data);
+    var template = Handlebars.compile(data);
+    var params = {
+      title: 'test',
+      places: places
+    };
+    console.log(places);
+    var html = template(params);
+    console.log(html);
+    var box = Boundary.createBox('comment-box').html(html);
     Boundary.loadBoxCSS('#comment-box', chrome.extension.getURL('public/pure.min.css'));
-    Boundary.loadBoxCSS('#comment-box', chrome.extension.getURL('public/comment-box.css'));
+    Boundary.loadBoxCSS('#comment-box', chrome.extension.getURL('public/comments.css'));
   });
 }
